65c88433d2bbf353a08fb35894cf8a5586b7c3ac
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-10-07 17:27:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-red-500 h-screen w-screen flex items-center justify-center'>
+    APP
+    </div>
+  )
+}
+
+export default App
